d6d3d31666feae9a4a22ee3a2277043527ecfe03
Garr1X Bo <z1073223452@gmail.com>
2020-04-29 10:17:38 +0800

    gui & Teacher - Student

--- src/GUI/GuiTest.java
+++ src/GUI/GuiTest.java
@@ -2,6 +2,7 @@ package GUI;
 
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 class Person{
     private String name;
@@ -51,30 +52,73 @@ class Student extends Person{
 }
 
 public class GuiTest extends Frame implements ActionListener {
-    Frame f = new Frame("Test");                        //新建窗口
-    private Button Stu = new Button("Student");            //新建按钮
-    private Button Tea = new Button("Teacher");               //新建按钮
-    private TextArea ta = new TextArea(5,5);       //新建文本框
+    //新建窗口
+    JFrame f = new JFrame("Test");
+    //新建按钮
+    private JButton Stu = new JButton("Student");
+    //新建按钮
+    private JButton Tea = new JButton("Teacher");
+    //新建文本框
+    private JTextArea ta = new JTextArea(5,5);
+    //新建菜单栏
+    private JMenuBar mb = new JMenuBar();
+    //新建菜单
+    private JMenu m1 = new JMenu("m1");
+    private JMenu m2 = new JMenu("m2");
+    private JMenu m3 = new JMenu("m3");
+    private JMenu m4 = new JMenu("m4");
+    //新建菜单项
+    private JMenuItem m11 = new JMenu("F");
+    private JMenuItem m12 = new JMenu("C");
     public void init(){
-        Tea.addActionListener(this);                         //添加事件监控器
+        //添加事件监控器
+        Tea.addActionListener(this);
         Stu.addActionListener(this);
-        f.add(ta);                                              //窗口中添加
+        /*
+        m1.addActionListener(this);
+        m11.addActionListener(this);
+        m12.addActionListener(this);
+         */
+        //菜单栏中加入菜单
+        mb.add(m1);
+        mb.add(m2);
+        mb.add(m3);
+        mb.add(m4);
+        //菜单中加入菜单项
+        m1.add(m11);
+        m1.add(m12);
+        //窗口中添加
+        f.setJMenuBar(mb);
+        f.add(ta);
         f.add(Tea,BorderLayout.SOUTH);
         f.add(Stu,BorderLayout.NORTH);
-        f.pack();                                               //自动调整框架位置
+        //自动调整框架位置
+        f.pack();
         f.setVisible(true);
     }
     @Override
-    public void actionPerformed(ActionEvent e){                 //重写actionPerformed方法
+    //重写actionPerformed方法
+    public void actionPerformed(ActionEvent e){
         String name = e.getActionCommand();
         Student s = new Student("Zhong",20,"CS&T",111);
-        Teacher t = new Teacher("Liu",46,"Java",12120);//获取按键名称
+        Teacher t = new Teacher("Liu",46,"Java",12120);
+        //获取按键名称
         if("Student".equals(name)){
             ta.setText("Name:" + s.getName() + "\nAge:" + s.getAge() + "\nMajor:" + s.getMajor() + "\nStuNum:" + s.getSnum());
         }
         else if("Teacher".equals(name)){
             ta.setText("Name:" + t.getName() + "\nAge:" + t.getAge() + "\nTeachClass:" + t.getTeachClass() + "\nSaraly:" + t.getSaraly());
         }
+        /*未完成
+        else if(e.getSource()==m11){
+            System.out.println("m11");
+            System.exit(0);
+        }
+        else if(e.getSource()==m12){
+            System.out.println("m12");
+            System.exit(-1);
+        }
+        */
     }
     public static void main(String[] args){
         new GuiTest().init();
