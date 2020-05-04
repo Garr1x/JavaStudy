d6061ca7bf08dab6be941abdd359c20463f64b5f
Garr1X Bo <z1073223452@gmail.com>
2020-05-04 16:01:05 +0800

    Thread 多线程

--- src/ThreadTest/ThreadTest.java
+++ src/ThreadTest/ThreadTest.java
@@ -1,10 +1,10 @@
 package ThreadTest;
 
 class MyThead extends Thread{
-    private int ticket = 10;
+    private int ticket = 100;
     @Override
     public void run(){
-        for(int i=0;i<10;i--){
+        for(int i=0;i<100;i--){
             if(ticket>0){
                 System.out.println("Remaining votes:" + --ticket);
             }
@@ -14,7 +14,15 @@ class MyThead extends Thread{
 
 public class ThreadTest {
     public static void main(String[] args){
-        MyThead t = new MyThead();
-        t.run();
+        //实例化对象
+        MyThead t1 = new MyThead();
+        MyThead t2 = new MyThead();
+        MyThead t3 = new MyThead();
+        MyThead t4 = new MyThead();
+        //调用线程主体
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 }
